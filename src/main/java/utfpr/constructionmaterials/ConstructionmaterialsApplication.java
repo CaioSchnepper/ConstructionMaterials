760975f83316b4ce54ef17c74b12350aa9fb8b88
Caio Schnepper <caioschnepper@gmail.com>
2022-04-24 19:26:48 -0300

    initial commit with spring boot project

---
+++ src/main/java/utfpr/constructionmaterials/ConstructionmaterialsApplication.java
@@ -0,0 +1,13 @@
+package utfpr.constructionmaterials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionmaterialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionmaterialsApplication.class, args);
+	}
+
+}
