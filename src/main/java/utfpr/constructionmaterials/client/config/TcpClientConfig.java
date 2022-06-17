5da97205a77b2ebc10fd10884173c01aadd139ad
Caio Schnepper <caioschnepper@gmail.com>
2022-06-16 23:36:38 -0300

    feat: tentativas de retry e captura de socket fechado

--- src/main/java/utfpr/constructionmaterials/client/config/TcpClientConfig.java
+++ src/main/java/utfpr/constructionmaterials/client/config/TcpClientConfig.java
@@ -5,14 +5,18 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.handler.advice.ErrorMessageSendingRecoverer;
+import org.springframework.integration.handler.advice.RequestHandlerRetryAdvice;
 import org.springframework.integration.ip.tcp.TcpOutboundGateway;
-import org.springframework.integration.ip.tcp.connection.AbstractClientConnectionFactory;
-import org.springframework.integration.ip.tcp.connection.CachingClientConnectionFactory;
-import org.springframework.integration.ip.tcp.connection.TcpNioClientConnectionFactory;
+import org.springframework.integration.ip.tcp.connection.*;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import static utfpr.constructionmaterials.shared.constants.ClientConfigs.CONNECTION_TIMEOUT;
@@ -37,12 +41,53 @@ public class TcpClientConfig implements ApplicationEventPublisherAware {
         this.applicationEventPublisher = applicationEventPublisher;
     }
 
-    @Bean
+    //@Bean
     public AbstractClientConnectionFactory clientConnectionFactory() {
         TcpNioClientConnectionFactory tcpNioClientConnectionFactory = new TcpNioClientConnectionFactory(host, port);
         tcpNioClientConnectionFactory.setUsingDirectBuffers(true);
         tcpNioClientConnectionFactory.setApplicationEventPublisher(applicationEventPublisher);
         return new CachingClientConnectionFactory(tcpNioClientConnectionFactory, connectionPoolSize);
+    }
+
+    @Bean
+    public ThreadAffinityClientConnectionFactory threadBoundClient() {
+        return new ThreadAffinityClientConnectionFactory(clientConnectionFactory());
+    }
+
+    @Bean
+    public RequestHandlerRetryAdvice tcpRetryAdvice() {
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(3);
+
+        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+        backOffPolicy.setInitialInterval(100);
+        backOffPolicy.setMaxInterval(1000);
+        backOffPolicy.setMultiplier(3);
+
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(retryPolicy);
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+
+        RequestHandlerRetryAdvice tcpRetryAdvice = new RequestHandlerRetryAdvice();
+        tcpRetryAdvice.setRetryTemplate(retryTemplate);
+
+        // This allows fail-controlling
+        tcpRetryAdvice.setRecoveryCallback(new ErrorMessageSendingRecoverer(outboundChannel()));
+
+        return tcpRetryAdvice;
+    }
+
+    @EventListener
+    public void listen(TcpConnectionCloseEvent event) {
+        if (event.getConnectionFactoryName().equals("clientConnectionFactory")) {
+            try {
+                threadBoundClient().releaseConnection();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(event);
     }
 
     @Bean
